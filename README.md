71520c644ca77de9a7a1d57fbe670303c801dd84
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-08-06 15:12:17 -0400

    srv: adds initial main source file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sonic-httpd
 Sonic Speed HTTP Server
+
+## Build
+
+```sh
+gcc -Wall -Waextra -Wformat -g -gdwarf-4 -O0 main.cpp -o sonic-httpd.bin
+```
---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+
+sonic-httpd
+
+Copyright (C) 2026 Misael Díaz-Maldonado
+
+This file is released under the GNU General Public License version 2 only
+as published by the Free Software Foundation.
+
+*/
+
+int main () {
+	return 0;
+}
