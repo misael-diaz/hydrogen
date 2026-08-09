31a7a4b09146dcac01916fbec8f8aa83f63941d6
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-08-09 09:55:19 -0400

    lint: indents main-loop
    NOTES
    just indentation changes were performed with vim editor

--- main.cpp
+++ main.cpp
@@ -323,114 +323,115 @@ int main () {
 	while (running) {
 
 		if (request) {
-		struct sockaddr_in client = {};
-		socklen_t len = sizeof(struct sockaddr_in);
-		rc = accept(fd, (struct sockaddr*) &client, &len);
-		if (-1 == rc) {
-			if ((EAGAIN != errno) && (EWOULDBLOCK != errno)) {
-				fprintf(stderr, "%s\n", strerror(errno));
-				freeaddrinfo(ai);
-				_exit(1);
-			}
-		}
-		else {
-		int sockfd = rc;
-		fprintf(
-			stdout,
-			"client: %s port: %d\n",
-			inet_ntoa(client.sin_addr),
-			ntohs(client.sin_port)
-		);
-
-		int sw = 0;
-		ssize_t bytes_read = 0;
-		ssize_t bytes_total = 0;
-		size_t const chunk = 16;
-		char *p = (typeof(p)) head;
-		do {
-			ret = read(sockfd, p, chunk);
-			if (-1 == ret) {
-				if (EINTR != errno) {
+			struct sockaddr_in client = {};
+			socklen_t len = sizeof(struct sockaddr_in);
+			rc = accept(fd, (struct sockaddr*) &client, &len);
+			if (-1 == rc) {
+				if ((EAGAIN != errno) && (EWOULDBLOCK != errno)) {
 					fprintf(stderr, "%s\n", strerror(errno));
 					freeaddrinfo(ai);
 					_exit(1);
 				}
-				sw = 1;
 			}
 			else {
-				bytes_read = ret;
-				bytes_total += bytes_read;
-				p += bytes_read;
-				if (!bytes_read) {
-					sw = 0;
+				int sockfd = rc;
+				fprintf(
+						stdout,
+						"client: %s port: %d\n",
+						inet_ntoa(client.sin_addr),
+						ntohs(client.sin_port)
+				       );
+
+				int sw = 0;
+				ssize_t bytes_read = 0;
+				ssize_t bytes_total = 0;
+				size_t const chunk = 16;
+				char *p = (typeof(p)) head;
+				do {
+					ret = read(sockfd, p, chunk);
+					if (-1 == ret) {
+						if (EINTR != errno) {
+							fprintf(stderr, "%s\n", strerror(errno));
+							freeaddrinfo(ai);
+							_exit(1);
+						}
+						sw = 1;
+					}
+					else {
+						bytes_read = ret;
+						bytes_total += bytes_read;
+						p += bytes_read;
+						if (!bytes_read) {
+							sw = 0;
+						}
+						else if (chunk != bytes_read)
+							sw = 0;
+						else {
+							sw = 1;
+						}
+					}
+				} while (sw);
+
+				fprintf(stdout, "%s\n", "request header:");
+				fprintf(stdout, "%s", (char*) head);
+				fprintf(stdout, "bytes: %ld\n", bytes_total);
+
+				void *data = &sockfd;
+
+				errno = 0;
+				pid_t pid = clone(
+						respond,
+						top_stack,
+						CLONE_PTRACE | CLONE_FILES | SIGCHLD,
+						data
+						);
+				if (-1 == pid) {
+					// NOTE: in the future you may want to allow EAGAIN meaning that too many processes are already running we only need to wait a little longer
+					if (errno) {
+						fprintf(stderr, "%s\n", strerror(errno));
+					}
+					freeaddrinfo(ai);
+					_exit(1);
 				}
-				else if (chunk != bytes_read)
-					sw = 0;
-				else {
-					sw = 1;
-				}
+
 			}
-		} while (sw);
-
-		fprintf(stdout, "%s\n", "request header:");
-		fprintf(stdout, "%s", (char*) head);
-		fprintf(stdout, "bytes: %ld\n", bytes_total);
-
-		void *data = &sockfd;
-
-		errno = 0;
-		pid_t pid = clone(
-			respond,
-			top_stack,
-			CLONE_PTRACE | CLONE_FILES | SIGCHLD,
-			data
-		);
-		if (-1 == pid) {
-			// NOTE: in the future you may want to allow EAGAIN meaning that too many processes are already running we only need to wait a little longer
-			if (errno) {
-				fprintf(stderr, "%s\n", strerror(errno));
-			}
-			freeaddrinfo(ai);
-			_exit(1);
-		}
-
-		}
+			request = 0;
 		}
 
 		do {
 
-		errno = 0;
-		int wstatus = 0;
-		rc = waitpid(-1, &wstatus, WNOHANG);
-		if (-1 == rc) {
-			if (ECHILD != errno) {
-				fprintf(stderr, "%s\n", strerror(errno));
-				freeaddrinfo(ai);
-				_exit(1);
+			errno = 0;
+			int wstatus = 0;
+			rc = waitpid(-1, &wstatus, WNOHANG);
+			if (-1 == rc) {
+				if (ECHILD != errno) {
+					fprintf(stderr, "%s\n", strerror(errno));
+					freeaddrinfo(ai);
+					_exit(1);
+				}
+				sleep(1);
 			}
-			sleep(30);
-		}
-		else if (0 < rc) {
-			pid_t pid = rc;
-			if (WIFEXITED(wstatus)) {
-				fprintf(
-					stdout,
-					"pid: %d status: %d\n",
-					pid,
-					WEXITSTATUS(wstatus)
-				);
+			else if (0 < rc) {
+				pid_t pid = rc;
+				if (WIFEXITED(wstatus)) {
+					fprintf(
+							stdout,
+							"pid: %d status: %d\n",
+							pid,
+							WEXITSTATUS(wstatus)
+					       );
+				}
+				else if (WIFSIGNALED(wstatus)) {
+					fprintf(
+							stdout,
+							"pid: %d signal: %d\n",
+							pid,
+							WTERMSIG(wstatus)
+					       );
+				}
 			}
-			else if (WIFSIGNALED(wstatus)) {
-				fprintf(
-					stdout,
-					"pid: %d signal: %d\n",
-					pid,
-					WTERMSIG(wstatus)
-				);
-			}
-		}
 
-		} while (!request);
+		} while (running && !request);
 	}
 
 	freeaddrinfo(ai);
