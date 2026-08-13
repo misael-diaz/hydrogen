a23e21c7165af3c9bbf5ea54ba0d4358c3bb8c17
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-08-12 21:44:01 -0400

    srv: responds to favicon requests
    NOTES
    all that we need to know is the size in bytes of the favicon file to
    write the contents of the file to response
    
    we use a memory map to map the image contents to memory fast
    
    have tested the favicon endpoint in both chromium and firefox

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ as published by the Free Software Foundation.
 #include <linux/limits.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <netinet/in.h>
@@ -36,6 +37,8 @@ as published by the Free Software Foundation.
 #define HTTP_FAILURE_RC -1
 #define HTTP_LISTEN_PORT 8080
 #define HTTP_HEADER_SIZE (PATH_MAX)
+#define HTTP_CONTENT_LENGTH_SIZE 256
+#define HTTP_FAVICON_PATH "./public/favicons/favicon.png"
 
 // NOTE: this is how we disable function name mangling when compiling with a C++ compiler
 #ifndef __cplusplus
@@ -45,6 +48,32 @@ as published by the Free Software Foundation.
 #endif
 
 #define __httpd_internal __attribute__((visibility("hidden")))
+
+// NOTE: do not attemp to hotload if adding a new HTTP Method
+__httpd_extern
+enum _HttpMethodShifts {
+	HTTP_METHOD_HEAD_SHF = 0,
+	HTTP_METHOD_GET_SHF,
+	HTTP_METHOD_PUT_SHF,
+	HTTP_METHOD_POST_SHF,
+	HTTP_METHOD_DELETE_SHF,
+	// NOTE: Do **Not** add http methods after UNKNOWN so that we never miss the count
+	HTTP_METHOD_UNKNOWN_SHF,
+	HTTP_METHOD_LAST_SHF = HTTP_METHOD_DELETE_SHF,
+	HTTP_METHOD_COUNT = HTTP_METHOD_LAST_SHF,
+	HTTP_METHOD_NUM = HTTP_METHOD_COUNT,
+	HTTP_METHOD_MAX = HTTP_METHOD_COUNT
+};
+
+__httpd_extern
+enum HttpMethod {
+	HTTP_METHOD_HEAD = (1 << HTTP_METHOD_HEAD_SHF),
+	HTTP_METHOD_GET = (1 << HTTP_METHOD_GET_SHF),
+	HTTP_METHOD_PUT = (1 << HTTP_METHOD_PUT_SHF),
+	HTTP_METHOD_POST = (1 << HTTP_METHOD_POST_SHF),
+	HTTP_METHOD_DELETE = (1 << HTTP_METHOD_DELETE_SHF),
+	HTTP_METHOD_UNKNOWN = (1 << HTTP_METHOD_UNKNOWN_SHF)
+};
 
 static int request = 0;
 static int running = 0;
@@ -103,8 +132,10 @@ void HttpSignalHandler(int signum) {
 
 __httpd_extern
 __httpd_internal
-int HttpHeaderRead(int const sockfd) {
-	char head[HTTP_HEADER_SIZE];
+int HttpHeaderRead(
+	char * const head,
+	int const sockfd
+) {
 	int sw = 0;
 	ssize_t ret = 0;
 	ssize_t bytes_read = 0;
@@ -145,7 +176,223 @@ error_handler:
 	return HTTP_FAILURE_RC;
 }
 
+// NOTE: this is a very optimistic way of handling this and hence it needs improvement
+__httpd_extern
+__httpd_internal
+int HttpHeaderFindMethod(
+	enum HttpMethod * const method,
+	char const * const head,
+	char const ** const URI
+) {
+	int rc = HTTP_SUCCESS_RC;
+	char const * str = NULL;
+	char const HEAD[] = "HEAD";
+	char const GET[] = "GET";
+	char const PUT[] = "PUT";
+	char const POST[] = "POST";
+	char const DELETE[] = "DELETE";
+	if ((str = strstr(head, HEAD))) {
+		rc = HTTP_SUCCESS_RC;
+		str += sizeof(HEAD);
+		*URI = str;
+		*method = HTTP_METHOD_HEAD;
+	}
+	else if ((str = strstr(head, GET))) {
+		rc = HTTP_SUCCESS_RC;
+		str += sizeof(GET);
+		*URI = str;
+		*method = HTTP_METHOD_GET;
+	}
+	else if ((str = strstr(head, PUT))) {
+		rc = HTTP_SUCCESS_RC;
+		str += sizeof(PUT);
+		*URI = str;
+		*method = HTTP_METHOD_PUT;
+	}
+	else if ((str = strstr(head, POST))) {
+		rc = HTTP_SUCCESS_RC;
+		str += sizeof(POST);
+		*URI = str;
+		*method = HTTP_METHOD_POST;
+	}
+	else if ((str = strstr(head, DELETE))) {
+		rc = HTTP_SUCCESS_RC;
+		str += sizeof(DELETE);
+		*URI = str;
+		*method = HTTP_METHOD_DELETE;
+	}
+	else {
+		*URI = NULL;
+		rc = HTTP_FAILURE_RC;
+		*method = HTTP_METHOD_UNKNOWN;
+	}
+	return rc;
+}
+
+// NOTE: experimental code, this is so that we can respond with a favicon but this is not how I intend to handle responses
+// NOTE: this assumes that there are no more response header fields to include after this call
+// TODO: provide the response buffer size so that we know if there's enough space for the header
+// TODO: the data structure for the http response is starting to arise, we need an offset for the data and a size and a size for the header-section of the response
+__httpd_extern
+__httpd_internal
+int HttpRespondGetFavicon(
+	char * const response,
+	size_t * const bytes_response
+) {
+	int rc = 0;
+	int fd = -1;
+	ssize_t ret = -1;
+	size_t len = 0;
+	size_t len_written = 0;
+	size_t bytes_written = 0;
+	size_t bytes_favicon = 0;
+	size_t len_response = 0;
+	size_t avail_response = 0;
+	size_t size_response = 0;
+	size_t size_mmap = 0;
+	size_t pagesize = 0;
+	size_t pagemask = 0;
+	void *map = NULL;
+	char *img = NULL;
+	char *content = NULL;
+	char content_length[HTTP_CONTENT_LENGTH_SIZE];
+	struct stat st = {};
+
+	errno = 0;
+	ret = sysconf(_SC_PAGESIZE);
+	if (-1 == ret) {
+		if (errno) {
+			fprintf(stderr, "HttpRespondGetFavicon: %s\n", strerror(errno));
+		}
+	}
+	pagesize = (typeof(pagesize)) ret;
+	pagemask = (pagesize - 1);
+
+	errno = 0;
+	fd = open(HTTP_FAVICON_PATH, O_PATH | O_CLOEXEC, O_RDONLY);
+	if (-1 == fd) {
+		if (errno) {
+			fprintf(stderr, "HttpRespondGetFavicon: %s\n", strerror(errno));
+			goto error_handler;
+		}
+	}
+
+	errno = 0;
+	// NOTE: here we are being optimistic in that nobody is going to modify the favicon right after we get its size in bytes for the response
+	rc = fstat(fd, &st);
+	if (-1 == rc) {
+		if (errno) {
+			fprintf(stderr, "HttpRespondGetFavicon: %s\n", strerror(errno));
+			goto error_handler;
+		}
+	}
+
+	bytes_favicon = st.st_size;
+	bytes_written = snprintf(
+		content_length,
+		HTTP_CONTENT_LENGTH_SIZE,
+		"Content-Type: image/png\r\n"
+		"Content-Length: %lu\r\n"
+		"\r\n",
+		bytes_favicon
+	);
+
+	// NOTE: we need another file descriptor without O_PATH to mmap the contents (see `man open` and `man mmap`)
+	close(fd);
+	fd = -1;
+
+	errno = 0;
+	fd = open(HTTP_FAVICON_PATH, O_CLOEXEC, O_RDONLY);
+	if (-1 == fd) {
+		if (errno) {
+			fprintf(stderr, "HttpRespondGetFavicon: %s\n", strerror(errno));
+			goto error_handler;
+		}
+	}
+
+	if (HTTP_CONTENT_LENGTH_SIZE <= bytes_written) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error truncation");
+		goto error_handler;
+	}
+
+	len_written = strlen(content_length);
+	if (len_written != bytes_written) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error size of content-length buffer");
+		goto error_handler;
+	}
+
+	len_response = strlen(response);
+	size_response = 1 + len_response;
+	if (HTTP_HEADER_SIZE < size_response) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error header size");
+		goto error_handler;
+	}
+
+	avail_response = (HTTP_HEADER_SIZE - size_response);
+
+	if (avail_response < bytes_written) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error avail header size");
+		goto error_handler;
+	}
+
+	memcpy(response + len_response, content_length, bytes_written);
+	// NOTE: we include the null byte because we intend to keep it at the end of the response for the caller because it expects it and we use `len_response` to know where the image data begins
+	size_response += bytes_written;
+	len_response = size_response - 1;
+
+	// NOTE: as above here we add the content and so we must be sure to have enough room for the content
+	size_mmap = ((bytes_favicon + pagemask) & (~pagemask));
+
+	errno = 0;
+	map = mmap(NULL, size_mmap, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (MAP_FAILED == map) {
+		if (errno) {
+			fprintf(stderr, "HttpRespondGetFavicon: %s\n", strerror(errno));
+			goto error_handler;
+		}
+	}
+
+	// NOTE: the memory mapping should have cleared the trailing bytes to zero and so if we try to access the data at the boundary we should get the null byte (zero)
+	img = (typeof(img)) map;
+	if (img[bytes_favicon]) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error: image size mismatch detected");
+		goto error_handler;
+	}
+
+	if (HTTP_HEADER_SIZE < size_response) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error content size");
+		goto error_handler;
+	}
+
+	// NOTE: size response includes the null byte that the caller expects at the end
+	avail_response = (HTTP_HEADER_SIZE - size_response);
+
+	if (avail_response < bytes_favicon) {
+		fprintf(stderr, "HttpRespondGetFavicon: %s\n", "error avail content size");
+		goto error_handler;
+	}
+
+	// NOTE: we intend to overwrite the null character that strncat appended and so we expect no changes to `len_response` between calling `strncat` and `memcpy` and we append a null byte for the caller because it expects it
+	content = response + len_response;
+	memcpy(content, img, bytes_favicon);
+	len = bytes_favicon;
+	content[len] = 0;
+
+	len = len_response + bytes_favicon;
+	*bytes_response = len;
+	rc = HTTP_SUCCESS_RC;
+	return rc;
+error_handler:
+	if (0 < fd) {
+		close(fd);
+		fd = -1;
+	}
+	rc = HTTP_FAILURE_RC;
+	return rc;
+}
+
 // TODO:
+// [ ] check if closing the socket on errors would make the client hang (note that we are not responding just closing the connection)
 // [ ] RFC9112 https://www.rfc-editor.org/info/rfc9112/#name-message-body reject requests with both Content-Length and Transfer-Enconding and close the connection.
 // [ ] A server MAY reject a request that contains a message body but not a Content-Length by responding with 411 (Length Required). https://www.rfc-editor.org/info/rfc9112/#section-6.3-5
 // [ ] Host field is required in HTTP Requests to HTTP/1.1 servers; complain by responding with a status 400 "(Bad Request)" if the Host field is missing from the request header. https://www.rfc-editor.org/info/rfc9112/#section-3.2-4
@@ -159,13 +406,17 @@ __httpd_internal
 int HttpRespond(void *data) {
 	struct ClientData *client = (typeof(client)) data;
 	int fd = client->sockfd;
+	enum HttpMethod method = HTTP_METHOD_UNKNOWN;
+	size_t bytes_response = 0;
+
+	char const *URI = NULL;
+	char head[HTTP_HEADER_SIZE];
 
 	char CRLF[] = "\r\n";
 
 	// TODO: we probably want to clear define this later, so we should only allocate
 	char response[HTTP_HEADER_SIZE] = (
 		"HTTP/1.1 200 \r\n"
-		"Content-Length: 0\r\n"
 		"Connection: close\r\n"
 	);
 
@@ -213,8 +464,6 @@ int HttpRespond(void *data) {
 	}
 
 	strncat(response, timestamp, bytes_time);
-	strncat(response, CRLF, sizeof(CRLF) - 1);
-	// NOTE: for now this is the end of the response and so we add the final CRLF
 	strncat(response, CRLF, sizeof(CRLF) - 1);
 
 	ssize_t bytes_written = 0;
@@ -270,15 +519,54 @@ int HttpRespond(void *data) {
 		goto error_handler;
 	}
 
-	rc = HttpHeaderRead(fd);
+	memset(head, 0, sizeof(head));
+	rc = HttpHeaderRead(head, fd);
+	// FIXME: check againt HTTP_FAILURE_RC instead
 	if (-1 == rc) {
 		fprintf(stderr, "%s\n", "HttpHeaderRead: read header failed");
 		goto error_handler;
 	}
 
-	// NOTE: we are effectively excluding the terminating null byte from the response
+	rc = HttpHeaderFindMethod(&method, head, &URI);
+	if (HTTP_FAILURE_RC == rc) {
+		fprintf(stderr, "%s\n", "HttpHeaderFindMethod: unexpected failure");
+		goto error_handler;
+	}
+	else if (HTTP_METHOD_UNKNOWN == method) {
+		fprintf(stderr, "%s\n", "HttpHeaderFindMethod: error unknown method detected");
+		goto error_handler;
+	}
+	else if (!URI) {
+		fprintf(stderr, "%s\n", "HttpHeaderFindMethod: uninitialized URI");
+		goto error_handler;
+	}
+
+	// TODO: refactor this into the router function
+	bytes_response = 0;
+	if (HTTP_METHOD_GET == method) {
+		if (strstr(URI, "favicon")) {
+			rc = HttpRespondGetFavicon(response, &bytes_response);
+			if (HTTP_FAILURE_RC == rc) {
+				goto error_handler;
+			}
+		}
+		else {
+			// NOTE: generates the original default response
+			char content_length[] = (
+				"Content-Length: 0\r\n"
+				"\r\n"
+			);
+			strncat(response, content_length, sizeof(content_length) - 1);
+		}
+	}
+
+	// NOTE: we are effectively excluding the terminating null byte from the response and note that this only works for pure text responses and this check will be revised in the future because it's insufficient
 	errno = 0;
-	bytes_written = write(fd, response, strlen(response));
+	if (!bytes_response) {
+		bytes_written = write(fd, response, strlen(response));
+	} else {
+		bytes_written = write(fd, response, bytes_response);
+	}
 	if (-1 == bytes_written) {
 		if (errno) {
 			fprintf(stderr, "%s\n", strerror(errno));
@@ -536,7 +824,7 @@ int main () {
 	size_t const pagesize = ret;
 
 	errno = 0;
-	size_t const size_stack = (((HTTP_HEADER_SIZE) + (pagesize)) << 1);
+	size_t const size_stack = (((HTTP_HEADER_SIZE) + (pagesize << 1)) << 1);
 	void *stack = mmap(NULL,
 			size_stack,
 			PROT_READ | PROT_WRITE,
