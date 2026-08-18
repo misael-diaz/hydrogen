a207bab38430881ca407b902e89b7ef4f2ed86d0
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-08-18 14:17:04 -0400

    mod: adds root module

---
+++ root.cpp
@@ -0,0 +1,34 @@
+/*
+
+sonic-httpd
+
+Copyright (C) 2026 Misael Díaz-Maldonado
+
+This file is released under the GNU General Public License version 2 only
+as published by the Free Software Foundation.
+
+*/
+
+#include "http.hpp"
+
+#define HTTP_URI_ROOT "/"
+#define HTTP_PATH_ROOT (DIRBUILD "/http/index.html")
+
+__httpd_extern
+__httpd_internal
+int RootGet(
+        struct HttpRequest const * const DataRequest __attribute__((unused)),
+        struct HttpResponse * const DataResponse
+) {
+        return HttpRespondGetFile(DataResponse, HTTP_PATH_ROOT);
+}
+
+// NOTE: not going to include stddef.h just for NULL, we can use zero instead
+struct HttpModule rootModule = {
+	.name = HTTP_URI_ROOT,
+	.Head = 0,
+	.Get = RootGet,
+	.Put = 0,
+	.Post = 0,
+	.Delete = 0
+};
